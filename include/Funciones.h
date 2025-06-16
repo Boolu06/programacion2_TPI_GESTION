1820adfbfb998474d4edeee432b0fe2ab26cd5e1
Nelson Adarmes <jhoeladarmes@gmail.com>
2025-06-16 17:17:19 -0300

    modificaciones en la funcion de mostrar de cada objeto

--- include/Funciones.h
+++ include/Funciones.h
@@ -16,6 +16,7 @@ bool esSoloNumeros(std::string numero);
 bool esSoloNumeros(float numero);
 bool esSoloLetras(std::string palabra);
 std::string aMinusculas(std::string palabra);
-
-
-
+std::string llenarEspaciosString(std::string cadena,int espaciosTotales);
+std::string llenarEspaciosInt(int cifra, int espaciosTotales);
+std::string llenarEspaciosFloat(float cifra, int espaciosTotales);
+std::string llenarEspaciosFecha(int dia, int mes, int anio, int espaciosTotales);
